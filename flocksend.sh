a9f32f074b0f2b4ac28ae0dbfd636b680e2e89f0
DannyHarding <danny@stuntrocket.co>
2021-03-08 23:09:05 +0000

    Initial

--- flocksend.sh
+++ flocksend.sh
@@ -4,13 +4,11 @@ START=$(date +%Y-%m-%d-%H-%M)
 HOSTNAME=$(hostname | awk '{print toupper($0)}')
 THIS=$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || echo "$0")
 DIR=$(dirname "${THIS}")
-CONFIG_FILE="./config.conf"
+CONFIG_FILE="$DIR/config.conf"
 
 MESSAGE=${1:-"default message"}
 
 . $CONFIG_FILE
-
-
 
 PAYLOAD="{
   \"flockml\": \"<flockml>$HOSTNAME - $MESSAGE</flockml>\"
